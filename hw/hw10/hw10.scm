39ca6ed34ee58e0b0690f799edf1801173ddc473
pianus <le.yu@uconn.edu>
2018-09-17 09:05:39 -0600

    scheme finished

--- hw/hw10/hw10.scm
+++ hw/hw10/hw10.scm
@@ -1,14 +1,17 @@
 (define (accumulate combiner start n term)
   (if (= n 0)
       start
-      'YOUR-CODE-HERE
+      (accumulate combiner (combiner start (term n)) (- n 1) term)
   )
 )
 
 (define (accumulate-tail combiner start n term)
-  'YOUR-CODE-HERE
+  (if (= n 0)
+      start
+      (accumulate combiner (combiner start (term n)) (- n 1) term)
+  )
 )
 
 (define-macro (list-of expr for var in seq if filter-fn)
-  'YOUR-CODE-HERE
+   `(map (lambda (,var) ,expr) (filter (lambda (,var) ,filter-fn) ,seq))
 )
---
+++ lecture/disc08.py
@@ -0,0 +1,66 @@
+from operator import add, sub
+OPERATORS = {'+':add, '-':sub}
+class nil:
+    """Represents the special empty pair nil in Scheme."""
+    def __repr__(self):
+        return 'nil'
+    def __len__(self):
+        return 0
+    def __getitem__(self, i):
+        raise IndexError('Index out of range')
+    def map(self, fn):
+        return nil
+nil = nil()
+
+class Pair:
+    """Represents the built-in pair data structure in Scheme."""
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+    def __repr__(self):
+        return 'Pair({}, {})'.format(self.first, self.second)
+    def __len__(self):
+        return 1 + len(self.second)
+    def __getitem__(self, i):
+        if i == 0:
+            return self.first
+        return self.second[i-1]
+    def map(self, fn):
+        return Pair(fn(self.first), self.second.map(fn))
+
+def calc_eval(exp):
+    """Evaluates a Calculator expression represented as a Pair."""
+    if isinstance(exp, Pair):
+        return calc_apply(calc_eval(exp.first),
+                          list(exp.second.map(calc_eval)))
+    elif exp in OPERATORS:
+        return OPERATORS[exp]
+    else: # Atomic expressions
+        return exp
+
+def calc_apply(op, args):
+    """Applies an operator to a Pair of arguments."""
+    return op(*args)
+
+def calc_eval(exp):
+    if isinstance(exp, Pair):
+            return calc_apply(calc_eval(exp.first),
+                          list(exp.second.map(calc_eval)))
+    if exp == 'and':
+        return eval_and
+    elif exp in OPERATORS:
+        return OPERATORS[exp]
+    else: # Atomic expression
+        return exp
+
+def eval_and(*args):
+    if len(args) == 0:
+        return True
+    else:
+        for i in args:
+            if not i:
+                return False
+        return i
+
+
+#Taiil_call optimozation
