0caae4d3c8d4f29a4733b6136d156cff528e60ab
le yu <le.yu@colorado.edu>
2018-10-01 17:56:43 -0600

    hw12

--- hw/hw12/hw12.sql
+++ hw/hw12/hw12.sql
@@ -29,25 +29,40 @@ CREATE TABLE sizes AS
 
 -- The size of each dog
 CREATE TABLE size_of_dogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT DISTINCT name, size FROM dogs, sizes
+  WHERE dogs.height > sizes.min and dogs.height <= sizes.max
+  ORDER BY dogs.name;
 
 -- All dogs with parents ordered by decreasing height of their parent
 CREATE TABLE by_height AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.name from dogs as a, dogs as b, parents
+  WHERE a.name = parents.child and b.name = parents.parent
+  Order by b.height DESC;
 
 -- Filling out this helper table is optional
 CREATE TABLE siblings AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.name as name1, b.name as name2
+  FROM dogs as a, dogs as b, parents as c, parents as d
+  WHERE a.name = c.child and b.name = d.child and a.name <> b.name
+    and c.parent = d.parent and a.name < b.name;
 
 -- Sentences about siblings that are the same size
 CREATE TABLE sentences AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT name1 || ' and ' || name2 || ' are ' || a.size || ' siblings'
+  FROM siblings, size_of_dogs as a, size_of_dogs as b
+  WHERE name1 = a.name and name2 = b.name and a.size = b.size;
 
 -- Ways to stack 4 dogs to a height of at least 170, ordered by total height
 CREATE TABLE stacks_helper(dogs, stack_height, last_height);
 
 -- Add your INSERT INTOs here
+INSERT INTO stacks_helper(dogs, stack_height, last_height)
+  SELECT a.name ||', '|| b.name ||', '|| c.name ||', '|| d.name,
+         a.height + b.height + c.height + d.height,
+         d.height
+  FROM dogs as a, dogs as b, dogs as c, dogs as d
+  WHERE a.height < b.height and b.height < c.height and c.height < d.height;
 
 
 CREATE TABLE stacks AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT dogs, stack_height from stacks_helper where stack_height > 170 order by stack_height;
